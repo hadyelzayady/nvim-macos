8900b45f9bbb499cf016fea0b05628405e826d5a
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2022-07-04 14:36:44 +0200

    reset lspconfig

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -17,21 +17,14 @@ lsp_installer.setup({
 })
 
 
-local servers = {
-    "angularls", "gopls", "tsserver", "eslint", "flow", "bashls", "dockerls", "julials", "pylsp", "pyright",
-    "jedi_language_server", "jdtls", "sumneko_lua", "vimls", "html", "jsonls", "solargraph", "cssls",
-    "yamlls", "clangd", "ccls", "sqls", "denols", "graphql", "dartls", "dotls",
-    "kotlin_language_server", "nimls", "intelephense", "vuels", "phpactor", "omnisharp",
-    "r_language_server", "rust_analyzer", "terraformls", "svelte", "texlab", "clojure_lsp", "elixirls",
-    "sourcekit", "fsautocomplete", "vls", "hls"
-}
+local servers = { 'eslint', 'tsserver', 'sumneko_lua' , 'jdtls'}
 
 local coq = require "coq"
 
 for _, lsp in pairs(servers) do
     require('lspconfig')[lsp].setup(coq.lsp_ensure_capabilities({
         on_attach = require('lsp.handlers').on_attach,
-        -- capabilities = require("lsp.handlers").capabilities,
+		capabilities = require("lsp.handlers").capabilities,
         flags = {
             -- This will be the default in neovim 0.7+
             debounce_text_changes = 150,
