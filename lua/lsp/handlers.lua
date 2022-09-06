a311bb444f74446e2c6d6885c2ab6696396425a5
hady.elzayady <hady.elzayady@pixelogicmedia.com>
2022-09-06 14:04:01 +0200

    disble auto format

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -72,13 +72,13 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 M.on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end
-        })
+        -- vim.api.nvim_create_autocmd("BufWritePre", {
+        --     group = augroup,
+        --     buffer = bufnr,
+        --     callback = function()
+        --         lsp_formatting(bufnr)
+        --     end
+        -- })
     end
     lsp_keymaps(bufnr)
 end
